6d0db1d31b2756922e10d61c39e2d9df3e7dedb5
Your Name <you@example.com>
2024-02-19 10:39:49 +0300

    cleaned the codes

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,4 @@
 import { defineStore } from "pinia";
-import axios from "axios";
-import { isAllowed } from "@/main";
 
 export const useComplaintsStore = defineStore("complaints", {
   state: () => ({
@@ -9,19 +7,5 @@ export const useComplaintsStore = defineStore("complaints", {
     API: "https://complaintapi.kodunya.com",
   }),
 
-  actions: {
-    async fetchData() {
-      try {
-        const response = await axios.get(`${this.API}/api/Complaints`, {
-          headers: {
-            Authorization: `Bearer ${isAllowed}`,
-            "Content-Type": "application/json",
-          },
-        });
-        this.complaints = response.data;
-      } catch (error) {
-        console.error("Error fetching complaints:", error);
-      }
-    },
-  },
+  actions: {},
 });
